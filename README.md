3bd57f6c13f318f0ba26d685c1ee6683e3f6ee1b
Ann Duong <thuyanannduong@gmail.com>
2021-02-22 11:34:07 -0500

    added first feature: gifs

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
     * News API
     * Yelp API
 
+3 Pillars: 
+When some action happens, make a fetch request, manipulate the DOM
+
+# Using the Browser vs Postman to make API calls
+
+
 ## Query Parameters
 * limit
 * pagnation 
@@ -18,8 +24,16 @@
 * `.map()`
 * `.filter()`
 
-## Async Await Syntax
-* A synchronous-likek syntax to write your async code!
+## Request Headers
+* When we fetch, we can fetch with an options objects
+* `fetch(url, optionsObject)`
+* Options objects includes key-value pairs like 
+    * `method` the HTTP verb for this request
+    * `headers` additional information about this request
 
 ## Bearer Tokens
 * Another form of Authorization and Authentication 
+
+## Async Await Syntax
+* A synchronous-like syntax to write your async code!
+* An `async` function _always_ returns a promise
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("connected")
+//When the giphy form is submitted, fetch to the GIPHY API, add <img> to our #giphy-div
+document.addEventListener("DOMContentLoaded", () => {
+  let gifsForm = document.querySelector("#giphy-form")
+  gifsForm.addEventListener("submit", fetchGifs);
+})
+
+function fetchGifs(event){
+  event.preventDefault()
+  let searchTerm = event.target[0].value
+  console.log(searchTerm)
+  fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEYS.giphy}&q=${searchTerm}&limit=3&rating=g`)
+  .then(res => { return res.json() })
+  .then(data => { 
+    let imageTags = processGifs(data).join("\n")
+    document.querySelector("#giphy-div").innerHTML = imageTags
+
+   })
+}
+
+function processGifs(obj){
+  return obj.data.map(gif => `<img src="${gif.images.original.url}" width="200px"/>`)
+}
